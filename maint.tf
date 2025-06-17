89a29b2c67e81161a017c10f286eca9a08fbee44
Sam <vuillaumes@gmail.com>
2025-06-17 18:12:58 -0400

    Update maint.tf

--- maint.tf
+++ maint.tf
@@ -2,4 +2,16 @@ resource "aws_instance" "bad_ec2" {
   ami                    = "ami-0c55b159cbfafe1f0"
   instance_type          = "t2.micro"
   associate_public_ip_address = true  # <-- public IP triggers critical
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "ROOT_PASSWORD=SuperSecret123" > /var/www/html/root-password.txt
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              EOF
+
+  tags = {
+    Name = "bad-ec2-exposed-password"
+  }
 }
