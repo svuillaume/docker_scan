7ac9131ca26b00cb239fe7642907320743a5caa5
Sam <vuillaumes@gmail.com>
2025-06-17 18:28:45 -0400

    Update maint.tf

--- maint.tf
+++ maint.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "bad_ec2" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "ROOT_PASSWORD=rootSecret1234" > /var/www/html/root-password.txt
+              echo "ROOT_PASSWORD=rootSecret123456" > /var/www/html/root-password.txt
               yum install -y httpd
               systemctl start httpd
               systemctl enable httpd
