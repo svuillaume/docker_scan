380c1f146d77793d7d999d57a13aea7044b790cd
Sam <vuillaumes@gmail.com>
2025-07-14 10:05:22 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ data "aws_vpc" "default" {
 
 resource "aws_security_group" "samv_sg" {
   name        = "samv-demo-sg"
-  description = "Allow all traffic"
+  description = "Allow all traffic (for testing)"
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
@@ -46,13 +46,55 @@ data "aws_ami" "ubuntu_old" {
   }
 }
 
+resource "aws_iam_role" "samv_full_admin" {
+  name = "samv-full-admin-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "admin_attach" {
+  role       = aws_iam_role.samv_full_admin.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_instance_profile" "samv_admin_profile" {
+  name = "samv-admin-profile"
+  role = aws_iam_role.samv_full_admin.name
+}
+
 resource "aws_instance" "samv_instance" {
-  ami                    = data.aws_ami.ubuntu_old.id
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.samv_key.key_name
-  vpc_security_group_ids = [aws_security_group.samv_sg.id]
+  ami                         = data.aws_ami.ubuntu_old.id
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.samv_key.key_name
+  vpc_security_group_ids      = [aws_security_group.samv_sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.samv_admin_profile.name
+
+  root_block_device {
+    volume_type           = "gp2"
+    volume_size           = 8
+    delete_on_termination = true
+    encrypted             = false
+  }
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "optional"
+    http_put_response_hop_limit = 1
+  }
 
   tags = {
-    Name = "samv-demo-only"
+    Name    = "samv-demo-only"
+    Purpose = "Vulnerability Testing"
   }
 }
