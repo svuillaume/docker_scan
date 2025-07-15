a02b9398d55f1af887f057f05a1db604d71d0188
Sam <vuillaumes@gmail.com>
2025-07-14 22:39:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,48 @@
 provider "aws" {
-  region = "ca-central-1"
+  region = "us-east-1"
 }
 
-resource "aws_security_group" "insecure_sg" {
-  name        = "insecure_sg"
-  description = "Security group with wide open rules"
-  vpc_id      = "vpc-12345678"  # dummy VPC
+# 1. Public S3 bucket
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "extremely-insecure-public-bucket-demo"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+  }
+
+  tags = {
+    Name        = "VulnerablePublicBucket"
+    Environment = "Test"
+  }
+}
+
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.public_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Sid       = "PublicReadGetObject",
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = "s3:GetObject",
+      Resource  = "${aws_s3_bucket.public_bucket.arn}/*"
+    }]
+  })
+}
+
+# 2. Insecure security group
+resource "aws_security_group" "open_sg" {
+  name        = "allow_all_inbound"
+  description = "Allow everything inbound"
+  vpc_id      = "vpc-xxxxxxxx" # <-- Replace with your VPC ID
 
   ingress {
     from_port   = 0
     to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Wide open to the world
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -20,31 +51,28 @@ resource "aws_security_group" "insecure_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "InsecureSG"
-  }
 }
 
-resource "aws_security_group_rule" "inbound_all" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]  # Open SSH
-  security_group_id = aws_security_group.insecure_sg.id
-}
-
-resource "aws_instance" "bad_ec2" {
-  ami                    = "ami-0c55b159cbfafe1f0"  # dummy AMI
+# 3. EC2 with hardcoded root password and unencrypted EBS
+resource "aws_instance" "vulnerable_ec2" {
+  ami                    = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI (us-east-1)
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.insecure_sg.id]
-  user_data              = <<EOF
-#!/bin/bash
-echo "SECRET=hardcodedsecretkey" > /etc/secrets.txt
-EOF
+  subnet_id              = "subnet-xxxxxxxx" # <-- Replace with your Subnet ID
+  vpc_security_group_ids = [aws_security_group.open_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "root:SuperInsecurePassword123!" | chpasswd
+              EOF
+
+  root_block_device {
+    volume_size = 8
+    volume_type = "gp2"
+    delete_on_termination = true
+    encrypted = false  # <-- Unencrypted disk
+  }
 
   tags = {
-    Name = "BadEC2Instance"
+    Name = "InsecureEC2"
   }
 }
